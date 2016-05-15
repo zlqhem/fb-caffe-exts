96060805a72337c2af909cbe82382f5300740bb1
Cysu <st.cysu@gmail.com>
2016-05-15 09:55:48 +0800

    Adapt the conv1 weights for caffe preprocessing

--- torch2caffe/prep.lua
+++ torch2caffe/prep.lua
@@ -4,6 +4,17 @@ require 'cudnn'
 
 local trans = require 'torch2caffe.transforms'
 
+local function adapt_conv1(layer)
+  local std = torch.FloatTensor({0.229, 0.224, 0.225}) * 255
+  local sz = layer.weight:size()
+  sz[2] = 1
+  layer.weight = layer.weight:cdiv(std:view(1,3,1,1):repeatTensor(sz))
+  local tmp = layer.weight:clone()
+  tmp[{{}, 1, {}, {}}] = layer.weight[{{}, 3, {}, {}}]
+  tmp[{{}, 3, {}, {}}] = layer.weight[{{}, 1, {}, {}}]
+  layer.weight = tmp:clone()
+end
+
 g_t2c_preprocess = function(model, opts)
   model = cudnn.convert(model, nn)
   --model = trans.fold_batch_normalization_layers(model, opts)
@@ -25,6 +36,7 @@ g_t2c_preprocess = function(model, opts)
     layer.running_mean = layer.running_mean:float()
     layer.running_var = layer.running_var:float()
   end
+  adapt_conv1(model.modules[1])
   return model
 end
 
